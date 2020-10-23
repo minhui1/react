51d394b8457a477b9ed8f2ac5d1c667225d10aae
minhui <minhui@minhuideMacBook-Pro.local>
2020-10-23 12:00:07 +0800

    package.json

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <h3>This is React App.</h3>
+      </div>
+      <div className="App">
+        <h3>This is React App-two.</h3>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
